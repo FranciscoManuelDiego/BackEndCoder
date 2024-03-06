d1c824c1744fc92ff20622df02b9210db4866133
FranciscoManuelDiego <franddiego@gmail.com>
2024-03-06 17:56:34 -0300

    Adding Sort

--- src/routes/viewsRoutes.js
+++ src/routes/viewsRoutes.js
@@ -24,8 +24,17 @@ router.get('/realtimeproducts', async (req, res) => {
     try {
         const page = parseInt(req.query.page) || 1;
         const limit = parseInt(req.query.limit) || 4;
+
+        // Check if a sorting parameter is present in the request
+        let sortQuery = { price: 1};
+        if (req.query.sort === 'price-asc') {
+             sortQuery = { price: 1 }; // Sort by price in ascending order
+        } else if (req.query.sort === 'price-desc') {
+            sortQuery = { price: -1 }; // Sort by price in descending order
+        }
+        
         // Directly call Product.paginate within the route handler
-        const products = await Product.paginate({}, { page, limit });
+        const products = await Product.paginate({}, { page, limit, sort:sortQuery });
         const viewData = {
             title: 'Realtime Products',
             products: products.docs,
